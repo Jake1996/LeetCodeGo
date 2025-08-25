0f1fbea3cba773dc9dc7796930e74ab37242a33b
Jake1996 <jatinpm96@gmail.com>
2025-08-25 23:21:12 +0530

    Update Coin Change Algorithm

--- solutions/coinchange.go
+++ solutions/coinchange.go
@@ -2,18 +2,18 @@ package solutions
 
 import (
 	"fmt"
+	"slices"
 )
 
 // https://leetcode.com/problems/coin-change/description/
 
 func coinChange(coins []int, amount int) int {
 	change := make([]int, amount+1)
+	slices.Sort(coins)
 	for i := 1; i < len(change); i++ {
 		change[i] = 9999999
-		for j := 0; j < len(coins); j++ {
-			if coins[j] <= i {
-				change[i] = min(change[i], change[i-coins[j]]+1)
-			}
+		for j := 0; j < len(coins) && coins[j] <= i; j++ {
+			change[i] = min(change[i], change[i-coins[j]]+1)
 		}
 	}
 	if change[amount] == 9999999 {
