1fdfb13c7fce264034e7d9e4c9ee4bdc617004d6
Jake1996 <jatinpm96@gmail.com>
2025-08-28 10:32:44 +0530

    Rename Valiarbles

--- solutions/alloone.go
+++ solutions/alloone.go
@@ -23,52 +23,52 @@ func Constructor() AllOne {
 	return ret
 }
 
-func (this *AllOne) Inc(key string) {
-	e := this.hm[key]
+func (ao *AllOne) Inc(key string) {
+	e := ao.hm[key]
 	if e == nil {
 		v := &Value{
 			val:   key,
 			count: 1,
 		}
-		element := this.list.PushBack(v)
-		this.hm[key] = element
+		element := ao.list.PushBack(v)
+		ao.hm[key] = element
 	} else {
 		val := e.Value.(*Value)
 		val.count = val.count + 1
 		for e.Prev() != nil && e.Prev().Value.(*Value).count < val.count {
-			this.list.MoveBefore(e, e.Prev())
+			ao.list.MoveBefore(e, e.Prev())
 		}
 	}
 }
 
-func (this *AllOne) Dec(key string) {
-	e := this.hm[key]
+func (ao *AllOne) Dec(key string) {
+	e := ao.hm[key]
 	if e == nil {
 		return
 	} else {
-		e := this.hm[key]
+		e := ao.hm[key]
 		val := e.Value.(*Value)
 		if val.count == 1 {
-			this.list.Remove(e)
-			delete(this.hm, key)
+			ao.list.Remove(e)
+			delete(ao.hm, key)
 		}
 		val.count = val.count - 1
 		for e.Next() != nil && e.Next().Value.(*Value).count > val.count {
-			this.list.MoveAfter(e, e.Next())
+			ao.list.MoveAfter(e, e.Next())
 		}
 	}
 }
 
-func (this *AllOne) GetMaxKey() string {
-	if this.list.Front() != nil {
-		return this.list.Front().Value.(*Value).val
+func (ao *AllOne) GetMaxKey() string {
+	if ao.list.Front() != nil {
+		return ao.list.Front().Value.(*Value).val
 	}
 	return ""
 }
 
-func (this *AllOne) GetMinKey() string {
-	if this.list.Back() != nil {
-		return this.list.Back().Value.(*Value).val
+func (ao *AllOne) GetMinKey() string {
+	if ao.list.Back() != nil {
+		return ao.list.Back().Value.(*Value).val
 	}
 	return ""
 }
